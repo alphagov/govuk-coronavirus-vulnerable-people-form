eada1fdd718d54ab21212fbec58b0e8fb40bae38
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-03-20 14:49:00 +0000

    Add basic authentication
    I've added the required envvars to Heroku; it should
    work as normal locally.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  if ENV["REQUIRE_BASIC_AUTH"]
+    http_basic_authenticate_with(
+      name: ENV.fetch("BASIC_AUTH_USERNAME"),
+      password: ENV.fetch("BASIC_AUTH_PASSWORD"),
+    )
+  end
+
   private
 
   helper_method :previous_path
