7e368994cc88aa321a97bf1fc8ea345ef0522cfc
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-03-20 15:39:32 +0000

    Add "Do you have medical equipment to offer?" question
    This question is the first in the series of questions.  It has logic
    where a 'yes' response send the user through a further series of
    sub-questions about medical equipment.  If 'no' is selected, they are
    taken to the next question.

---
+++ app/controllers/coronavirus_form/medical_equipment_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class CoronavirusForm::MedicalEquipmentController < ApplicationController
+  include ActionView::Helpers::SanitizeHelper
+  include FieldValidationHelper
+
+  def show
+    session[:medical_equipment] ||= []
+    render "coronavirus_form/#{PAGE}"
+  end
+
+  def submit
+    medical_equipment = sanitize(params[:medical_equipment]).presence
+
+    session[:medical_equipment] = medical_equipment
+
+    invalid_fields = validate_radio_field(
+      PAGE,
+      radio: medical_equipment,
+    )
+
+    if invalid_fields.any?
+      flash.now[:validation] = invalid_fields
+      render "coronavirus_form/#{PAGE}"
+    elsif session["check_answers_seen"]
+      redirect_to controller: "coronavirus_form/check_answers", action: 'show'
+    elsif session[:medical_equipment] == I18n.t("coronavirus_form.medical_equipment.options.option_yes.label")
+      redirect_to controller: "coronavirus_form/medical_equipment_type", action: 'show'
+    else
+      redirect_to controller: "coronavirus_form/hotel_rooms", action: 'show'
+    end
+  end
+
+  private
+
+  PAGE = "medical_equipment"
+
+  def previous_path
+    "/"
+  end
+end
