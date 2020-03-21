fdfa67ba2681c971aa649ab5de886ff57c8c251b
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-03-21 11:00:32 +0000

    Remove all of the business form specific code
    The start page and check answers page have been left, as we will probably need them.
    
    For now the start page routes to the final check your answers page.

--- config/routes.rb
+++ config/routes.rb
@@ -8,31 +8,8 @@ Rails.application.routes.draw do
   get "/", to: "coronavirus_form/start#show"
 
   # Question pages
-  get "/coronavirus-form/medical-equipment" => "coronavirus_form/medical_equipment#show"
-  post "/coronavirus-form/medical-equipment" => "coronavirus_form/medical_equipment#submit"
-
-  get "/coronavirus-form/medical-equipment-type" => "coronavirus_form/medical_equipment_type#show"
-  post "/coronavirus-form/medical-equipment-type" => "coronavirus_form/medical_equipment_type#submit"
-
-  get "/coronavirus-form/hotel-rooms" => "coronavirus_form/hotel_rooms#show"
-  post "/coronavirus-form/hotel-rooms" => "coronavirus_form/hotel_rooms#submit"
-
-  get "/coronavirus-form/which-goods" => "coronavirus_form/which_goods#show"
-  post "/coronavirus-form/which-goods" => "coronavirus_form/which_goods#submit"
-
-  get "/coronavirus-form/which-services" => "coronavirus_form/which_services#show"
-  post "/coronavirus-form/which-services" => "coronavirus_form/which_services#submit"
-
-  get "/coronavirus-form/offer-food" => "coronavirus_form/offer_food#show"
-  post "/coronavirus-form/offer-food" => "coronavirus_form/offer_food#submit"
-
-  get "/coronavirus-form/offer-transport" => "coronavirus_form/offer_transport#show"
-  post "/coronavirus-form/offer-transport" => "coronavirus_form/offer_transport#submit"
 
   # Check answers page
   get "/coronavirus-form/check-your-answers" => "coronavirus_form/check_answers#show"
   post "/coronavirus-form/check-your-answers" => "coronavirus_form/check_answers#submit"
-
-  # Final page
-  get "/coronavirus-form/thank-you" => "coronavirus_form/thank_you#show"
 end
