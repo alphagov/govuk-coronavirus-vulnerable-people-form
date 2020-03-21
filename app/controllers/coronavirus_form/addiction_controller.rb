6106b517b7b85eaabfb503529582876682a7bd77
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-03-21 15:08:35 +0000

    Add "Do you have a history of addiction?" question

---
+++ app/controllers/coronavirus_form/addiction_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class CoronavirusForm::AddictionController < ApplicationController
+  include ActionView::Helpers::SanitizeHelper
+  include FieldValidationHelper
+
+  def show
+    render "coronavirus_form/#{PAGE}"
+  end
+
+  def submit
+    addiction = sanitize(params[:addiction]).presence
+    session[:addiction] = addiction
+
+    invalid_fields = validate_radio_field(
+      PAGE,
+      radio: addiction,
+    )
+
+    if invalid_fields.any?
+      flash.now[:validation] = invalid_fields
+      render "coronavirus_form/#{PAGE}"
+    elsif session["check_answers_seen"]
+      redirect_to controller: "coronavirus_form/check_answers", action: "show"
+    else
+      redirect_to controller: "coronavirus_form/#{NEXT_PAGE}", action: "show"
+    end
+  end
+
+private
+
+  PAGE = "addiction"
+  NEXT_PAGE = "check_answers"
+
+  def previous_path
+    "/"
+  end
+end
