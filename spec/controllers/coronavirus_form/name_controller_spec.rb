aa91b9d3b3d11325054d4c44ee28ec43c185e0b6
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-03-21 15:39:41 +0000

    Add question 4: what is your name?
    Route: /coronavirus-form/name
    https://trello.com/c/XIPwKE0a/33-what-is-your-name-page-q4

---
+++ spec/controllers/coronavirus_form/name_controller_spec.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe CoronavirusForm::NameController, type: :controller do
+  let(:current_template) { "coronavirus_form/name" }
+  let(:session_key) { :name }
+
+  describe "GET show" do
+    it "renders the form" do
+      get :show
+      expect(response).to render_template(current_template)
+    end
+  end
+
+  describe "POST submit" do
+    let(:params) do
+      {
+        "first_name" => "Harry",
+        "middle_name" => "Snape",
+        "last_name" => "Potter",
+      }
+    end
+    let(:person) do
+      {
+        "first_name" => "Harry",
+        "middle_name" => "Snape",
+        "last_name" => "Potter",
+      }
+    end
+
+
+    it "sets session variables" do
+      post :submit, params: params
+      expect(session[session_key]).to eq person
+    end
+
+    %w(first_name last_name).each do |field|
+      it "validates #{field} is required" do
+        post :submit, params: params.except(field)
+
+        expect(response).to render_template(current_template)
+      end
+
+      it "validates a value for #{field} is required" do
+        post :submit, params: params.merge(field => "")
+
+        expect(response).to render_template(current_template)
+      end
+    end
+
+    it "validates middle name is not required" do
+      post :submit, params: params.except("middle_name")
+
+      expect(session[session_key]).to eq person.merge("middle_name" => nil)
+      expect(response).to redirect_to("/")
+    end
+
+    it "validates a valid option is chosen" do
+      post :submit, params: { first_name: "<script></script>" }
+
+      expect(response).to render_template(current_template)
+    end
+
+    it "redirects to next step for a permitted response" do
+      post :submit, params: params
+      expect(response).to redirect_to("/")
+    end
+
+    it "redirects to check your answers if check your answers previously seen" do
+      session[:check_answers_seen] = true
+      post :submit, params: params
+
+      expect(response).to redirect_to(coronavirus_form_check_your_answers_path)
+    end
+  end
+end
