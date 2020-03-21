8a1631582f0fbcd31a365eeaf50ff30c3bfcbceb
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-03-21 15:16:00 +0000

    Add submit method to check your answers controller

--- app/controllers/coronavirus_form/check_answers_controller.rb
+++ app/controllers/coronavirus_form/check_answers_controller.rb
@@ -7,4 +7,23 @@ class CoronavirusForm::CheckAnswersController < ApplicationController
     session[:check_answers_seen] = true
     render "coronavirus_form/check_answers"
   end
+
+  def submit
+    submission_reference = reference_number
+
+    session[:reference_number] = submission_reference
+    FormResponse.create(form_response: session)
+
+    reset_session
+
+    redirect_to controller: "coronavirus_form/confirmation", action: "show", reference_number: submission_reference
+  end
+
+private
+
+  def reference_number
+    timestamp = Time.zone.now.strftime("%Y%m%d-%H%M%S")
+    random_id = SecureRandom.hex(3).upcase
+    "#{timestamp}-#{random_id}"
+  end
 end
--- spec/controllers/coronavirus_form/check_answers_controller_spec.rb
+++ spec/controllers/coronavirus_form/check_answers_controller_spec.rb
@@ -11,4 +11,34 @@ RSpec.describe CoronavirusForm::CheckAnswersController, type: :controller do
       expect(response).to render_template(current_template)
     end
   end
+
+  describe "POST submit" do
+    before do
+      allow_any_instance_of(described_class).to receive(:reference_number).and_return("abc")
+    end
+
+    it "saves the form response to the database" do
+      session["attribute"] = "key"
+      post :submit
+
+      expect(FormResponse.first.form_response).to eq(
+        [%w[attribute key], %w[reference_number abc]],
+      )
+    end
+
+    it "resets session" do
+      post :submit
+      expect(session.to_hash).to eq({})
+    end
+
+    it "redirects to thank you if sucessfully creates record" do
+      post :submit
+
+      expect(response).to redirect_to({
+        controller: "confirmation",
+        action: "show",
+        params: { reference_number: "abc" },
+      })
+    end
+  end
 end
