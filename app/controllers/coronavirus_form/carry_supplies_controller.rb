9b725b824a5b9f26ca143dae512c3a114fbfea54
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-03-21 16:35:11 +0000

    Add question: Carry supplies
    https://trello.com/c/5WB6IANw/41-can-deliveries-be-carried-inside-page

---
+++ app/controllers/coronavirus_form/carry_supplies_controller.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+class CoronavirusForm::CarrySuppliesController < ApplicationController
+  include ActionView::Helpers::SanitizeHelper
+  include FieldValidationHelper
+
+  def show
+    render "coronavirus_form/#{PAGE}"
+  end
+
+  def submit
+    carry_supplies = sanitize(params[:carry_supplies]).presence
+    session[:carry_supplies] = carry_supplies
+
+    invalid_fields = validate_radio_field(
+      PAGE,
+      radio: carry_supplies,
+    )
+
+    if invalid_fields.any?
+      flash.now[:validation] = invalid_fields
+      render "coronavirus_form/#{PAGE}"
+    elsif session["check_answers_seen"]
+      redirect_to controller: "coronavirus_form/check_answers", action: "show"
+    else
+      redirect_to controller: "coronavirus_form/#{NEXT_PAGE}", action: "show"
+    end
+  end
+
+private
+
+  PAGE = "carry_supplies"
+  NEXT_PAGE = "start" # TODO
+
+  def previous_path
+    "/" # TODO
+  end
+end
