d241153b33a94b5cf18c86a360207f2850a8b7a3
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-03-22 11:48:48 +0000

    Add Do you live in England page.

---
+++ app/controllers/coronavirus_form/live_in_england_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class CoronavirusForm::LiveInEnglandController < ApplicationController
+  include ActionView::Helpers::SanitizeHelper
+  include FieldValidationHelper
+
+  def show
+    render "coronavirus_form/#{PAGE}"
+  end
+
+  def submit
+    live_in_england = sanitize(params[:live_in_england]).presence
+    session[:live_in_england] = live_in_england
+
+    invalid_fields = validate_radio_field(
+      PAGE,
+      radio: live_in_england,
+    )
+
+    if invalid_fields.any?
+      flash.now[:validation] = invalid_fields
+      log_validation_error(invalid_fields)
+      render "coronavirus_form/#{PAGE}", status: :unprocessable_entity
+    elsif session["check_answers_seen"]
+      redirect_to controller: "coronavirus_form/check_answers", action: "show"
+    else
+      redirect_to controller: "coronavirus_form/#{NEXT_PAGE}", action: "show"
+    end
+  end
+
+private
+
+  PAGE = "live_in_england"
+  NEXT_PAGE = "nhs_letter"
+
+  def previous_path
+    "/"
+  end
+end
---
+++ spec/controllers/coronavirus_form/live_in_england_controller_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe CoronavirusForm::LiveInEnglandController, type: :controller do
+  let(:current_template) { "coronavirus_form/live_in_england" }
+  let(:session_key) { :live_in_england }
+
+  describe "GET show" do
+    it "renders the form" do
+      get :show
+      expect(response).to render_template(current_template)
+    end
+  end
+
+  describe "POST submit" do
+    let(:selected) { permitted_values.sample }
+    let(:permitted_values) do
+      I18n.t("coronavirus_form.questions.live_in_england.options").map { |_, item| item[:label] }
+    end
+
+    it "sets session variables" do
+      post :submit, params: { live_in_england: selected }
+      expect(session[session_key]).to eq selected
+    end
+
+    it "validates any option is chosen" do
+      post :submit, params: { live_in_england: "" }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response).to render_template(current_template)
+    end
+
+    it "validates a valid option is chosen" do
+      post :submit, params: { live_in_england: "<script></script>" }
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(response).to render_template(current_template)
+    end
+
+    it "redirects to next step for a permitted response" do
+      post :submit, params: { live_in_england: selected }
+      expect(response).to redirect_to(coronavirus_form_nhs_letter_path)
+    end
+
+    it "redirects to check your answers if check your answers previously seen" do
+      session[:check_answers_seen] = true
+      post :submit, params: { live_in_england: "Yes" }
+
+      expect(response).to redirect_to(coronavirus_form_check_your_answers_path)
+    end
+  end
+end
