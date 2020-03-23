53cc51700ee913bfa3fc6533e99f1aea39f1b701
Matt Hobbs <matt@nooshu.com>
2020-03-23 00:03:11 +0000

    Increase static asset cache time from 1d to 1y

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   if ENV["RAILS_SERVE_STATIC_FILES"].present?
     config.public_file_server.enabled = true
     config.public_file_server.headers = {
-      "Cache-Control" => "public, s-maxage=86400, max-age=86400",
+      "Cache-Control" => "public, s-maxage=31536000, max-age=31536000",
       "Expires" => 1.day.from_now.to_formatted_s(:rfc822),
     }
   else
