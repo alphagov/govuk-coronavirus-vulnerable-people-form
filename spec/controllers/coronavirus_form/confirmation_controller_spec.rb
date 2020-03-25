4fc7c23b5d6d3289f536c81ccf020e6419a455ee
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-03-25 11:41:15 +0000

    Fix incorrect redirect on confirmation page

--- spec/controllers/coronavirus_form/confirmation_controller_spec.rb
+++ spec/controllers/coronavirus_form/confirmation_controller_spec.rb
@@ -3,14 +3,10 @@
 require "spec_helper"
 
 RSpec.describe CoronavirusForm::ConfirmationController, type: :controller do
-  include_examples "redirections"
-
   let(:current_template) { "coronavirus_form/confirmation" }
 
   describe "GET show" do
     it "renders the form" do
-      session[:live_in_england] = "Yes"
-
       get :show
       expect(response).to render_template(current_template)
     end
