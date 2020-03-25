a5ffffe1d4853128c462465a5247f45b5807ef9a
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2020-03-25 11:43:59 +0000

    Run cucumber specs in the pipeline
    NOTE: There are some significant trade-offs that you need to consider
    when reviewing this. I recommend a read of this description before looking at
    the code.
    
    We have some cucumber feature tests, which are a much nicer way of
    testing that the application is working than `curl --fail`.
    
    At the moment, these use cucumber-rails which handles starting a local
    copy of the application, and provides nice Rails-y features like being
    able to use path helpers (e.g. `live_in_england_path` instead of
    `/live-in-england`).
    
    You can't use cucumber-rails to test external websites, because it's
    tightly hooked in to Rack. This means the tests weren't suitable for
    running against something hosted in GOV.UK PaaS for example.
    
    Instead, we can use cucumber and the apparition driver directly. This
    lets us test external domains, but doesn't provide all the nice rails-y
    bits like cucumber-rails does. That makes it painful to use locally
    against a locally running app.
    
    What I've done here is tried to get the best of both worlds:
    
    If you run `rails cucumber` without setting `TEST_URL` you'll get
    cucumber-rails, and things work like they did before.
    
    If you run `TEST_URL=http://example.com rails cucumber` you'll get
    cucumber with the apparition driver, and it will talk to an external
    website.
    
    The drawback of this is that the `step_definitions` will have to use
    lowest-common-denominator features - if you try and use something from
    cucumber-rails it will work locally, and in Travis but fail on
    concourse.
    
    I think that's probably a reasonable trade-off given how much better
    these tests would be than curl, but happy to discuss.
    
    Also note I've only tested a couple of pages - we should probably do the
    rest soon.

--- features/step_definitions/basic_care_needs_steps.rb
+++
@@ -1,8 +0,0 @@
-When("I visit the basic care needs page") do
-  visit basic_care_needs_path
-end
-
-Then("I will be redirected to the dietary requirements page") do
-  expect(page.status_code).to eq(200)
-  expect(page).to have_current_path(dietary_requirements_path)
-end
