174a3aef8eeaf533fc794f7bab1c7371409a50c1
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-03-25 12:21:17 +0000

    Add feature test for redirection

--- features/step_definitions/live_in_england_steps.rb
+++
@@ -1,8 +0,0 @@
-When("I visit the live in England page") do
-  visit live_in_england_path
-end
-
-Then("I will be redirected to the NHS letter page") do
-  expect(page.status_code).to eq(200)
-  expect(page).to have_current_path(nhs_letter_path)
-end
