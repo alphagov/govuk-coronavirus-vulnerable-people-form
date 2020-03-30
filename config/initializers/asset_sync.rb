2424460a6158f644342f324dbce794571f0af411
Sean Rankine <sean.rankine@digital.cabinet-office.gov.uk>
2020-03-30 16:18:05 +0100

    Add asset sync to copy assets to S3
    This adds the asset sync gem which will upload assets to S3 after
    assets:precompile.

---
+++ config/initializers/asset_sync.rb
@@ -0,0 +1,12 @@
+if defined?(AssetSync)
+  AssetSync.configure do |config|
+    config.fog_provider = "AWS"
+    config.fog_region = "eu-west-2"
+    config.fog_directory = ENV["AWS_ASSETS_BUCKET_NAME"]
+    config.aws_access_key_id = ENV["AWS_ACCESS_KEY_ID"]
+    config.aws_secret_access_key = ENV["AWS_SECRET_ACCESS_KEY"]
+
+    # Don't delete files from the store
+    config.existing_remote_files = "keep"
+  end
+end
