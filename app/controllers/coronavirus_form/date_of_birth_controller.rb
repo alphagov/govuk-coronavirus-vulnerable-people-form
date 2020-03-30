alphagov/govuk-coronavirus-find-support

Change date_of_birth children keys to symbol in session

app/controllers/coronavirus_form/date_of_birth_controller.rb
@@ -8,14 +8,14 @@ class CoronavirusForm::DateOfBirthController < ApplicationController
 
   def submit
     session[:date_of_birth] ||= {}
-    session[:date_of_birth]["day"] = sanitize(params.dig("date_of_birth", "day")&.strip).presence
-    session[:date_of_birth]["month"] = sanitize(params.dig("date_of_birth", "month")&.strip).presence
-    session[:date_of_birth]["year"] = sanitize(params.dig("date_of_birth", "year")&.strip).presence
+    session[:date_of_birth][:day] = sanitize(params.dig("date_of_birth", "day")&.strip).presence
+    session[:date_of_birth][:month] = sanitize(params.dig("date_of_birth", "month")&.strip).presence
+    session[:date_of_birth][:year] = sanitize(params.dig("date_of_birth", "year")&.strip).presence
 
     invalid_fields = validate_date_of_birth(
-      session[:date_of_birth]["year"],
-      session[:date_of_birth]["month"],
-      session[:date_of_birth]["day"],
+      session[:date_of_birth][:year],
+      session[:date_of_birth][:month],
+      session[:date_of_birth][:day],
       "date_of_birth",
     )
 

alphagov/govuk-coronavirus-vulnerable-people-form

app/controllers/coronavirus_form/date_of_birth_controller.rb
@@ -8,14 +8,14 @@ class CoronavirusForm::DateOfBirthController < ApplicationController
 
   def submit
     session[:date_of_birth] ||= {}
-    session[:date_of_birth]["day"] = strip_tags(params.dig("date_of_birth", "day")&.strip).presence
-    session[:date_of_birth]["month"] = strip_tags(params.dig("date_of_birth", "month")&.strip).presence
-    session[:date_of_birth]["year"] = strip_tags(params.dig("date_of_birth", "year")&.strip).presence
+    session[:date_of_birth][:day] = strip_tags(params.dig("date_of_birth", "day")&.strip).presence
+    session[:date_of_birth][:month] = strip_tags(params.dig("date_of_birth", "month")&.strip).presence
+    session[:date_of_birth][:year] = strip_tags(params.dig("date_of_birth", "year")&.strip).presence
 
     invalid_fields = validate_date_of_birth(
-      session[:date_of_birth]["year"],
-      session[:date_of_birth]["month"],
-      session[:date_of_birth]["day"],
+      session[:date_of_birth][:year],
+      session[:date_of_birth][:month],
+      session[:date_of_birth][:day],
       "date_of_birth",
     )
 