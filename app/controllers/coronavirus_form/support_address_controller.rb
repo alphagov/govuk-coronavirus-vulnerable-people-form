alphagov/govuk-coronavirus-find-support

Change support_address children keys to symbols in session

app/controllers/coronavirus_form/support_address_controller.rb
@@ -13,11 +13,11 @@ class CoronavirusForm::SupportAddressController < ApplicationController
 
   def submit
     session[:support_address] ||= {}
-    session[:support_address]["building_and_street_line_1"] = sanitize(params[:building_and_street_line_1]&.strip).presence
-    session[:support_address]["building_and_street_line_2"] = sanitize(params[:building_and_street_line_2]&.strip).presence
-    session[:support_address]["town_city"] = sanitize(params[:town_city]&.strip).presence
-    session[:support_address]["county"] = sanitize(params[:county]&.strip).presence
-    session[:support_address]["postcode"] = sanitize(params[:postcode]&.strip).presence
+    session[:support_address][:building_and_street_line_1] = sanitize(params[:building_and_street_line_1]&.strip).presence
+    session[:support_address][:building_and_street_line_2] = sanitize(params[:building_and_street_line_2]&.strip).presence
+    session[:support_address][:town_city] = sanitize(params[:town_city]&.strip).presence
+    session[:support_address][:county] = sanitize(params[:county]&.strip).presence
+    session[:support_address][:postcode] = sanitize(params[:postcode]&.strip).presence
 
     invalid_fields = validate_fields(session[:support_address])
 
@@ -41,21 +41,21 @@ private
     [
       validate_missing_fields(support_address),
       validate_conditionally_present_fields(support_address),
-      validate_postcode("postcode", support_address["postcode"]),
+      validate_postcode("postcode", support_address[:postcode]),
     ].flatten.uniq
   end
 
   def validate_conditionally_present_fields(product)
-    return [] if product["postcode"].present? || product["county"].present?
+    return [] if product[:postcode].present? || product[:county].present?
 
     invalid_fields = []
 
-    if product["county"].blank?
+    if product[:county].blank?
       invalid_fields << {
         field: "county",
         text: t("coronavirus_form.errors.missing_county_or_postcode_field"),
       }
-    elsif product["postcode"].blank?
+    elsif product[:postcode].blank?
       invalid_fields << {
         field: "postcode",
         text: t("coronavirus_form.errors.missing_county_or_postcode_field"),
@@ -67,7 +67,7 @@ private
 
   def validate_missing_fields(product)
     REQUIRED_FIELDS.each_with_object([]) do |field, invalid_fields|
-      next if product[field].present?
+      next if product[field.to_sym].present?
 
       invalid_fields << {
         field: field.to_s,

alphagov/govuk-coronavirus-vulnerable-people-form

app/controllers/coronavirus_form/support_address_controller.rb
@@ -13,11 +13,11 @@ class CoronavirusForm::SupportAddressController < ApplicationController
 
   def submit
     session[:support_address] ||= {}
-    session[:support_address]["building_and_street_line_1"] = strip_tags(params[:building_and_street_line_1]&.strip).presence
-    session[:support_address]["building_and_street_line_2"] = strip_tags(params[:building_and_street_line_2]&.strip).presence
-    session[:support_address]["town_city"] = strip_tags(params[:town_city]&.strip).presence
-    session[:support_address]["county"] = strip_tags(params[:county]&.strip).presence
-    session[:support_address]["postcode"] = strip_tags(params[:postcode]&.strip).presence
+    session[:support_address][:building_and_street_line_1] = strip_tags(params[:building_and_street_line_1]&.strip).presence
+    session[:support_address][:building_and_street_line_2] = strip_tags(params[:building_and_street_line_2]&.strip).presence
+    session[:support_address][:town_city] = strip_tags(params[:town_city]&.strip).presence
+    session[:support_address][:county] = strip_tags(params[:county]&.strip).presence
+    session[:support_address][:postcode] = strip_tags(params[:postcode]&.strip).presence
 
     invalid_fields = validate_fields(session[:support_address])
 
@@ -41,21 +41,21 @@ private
     [
       validate_missing_fields(support_address),
       validate_conditionally_present_fields(support_address),
-      validate_postcode("postcode", support_address["postcode"]),
+      validate_postcode("postcode", support_address[:postcode]),
     ].flatten.uniq
   end
 
   def validate_conditionally_present_fields(product)
-    return [] if product["postcode"].present? || product["county"].present?
+    return [] if product[:postcode].present? || product[:county].present?
 
     invalid_fields = []
 
-    if product["county"].blank?
+    if product[:county].blank?
       invalid_fields << {
         field: "county",
         text: t("coronavirus_form.errors.missing_county_or_postcode_field"),
       }
-    elsif product["postcode"].blank?
+    elsif product[:postcode].blank?
       invalid_fields << {
         field: "postcode",
         text: t("coronavirus_form.errors.missing_county_or_postcode_field"),
@@ -67,7 +67,7 @@ private
 
   def validate_missing_fields(product)
     REQUIRED_FIELDS.each_with_object([]) do |field, invalid_fields|
-      next if product[field].present?
+      next if product[field.to_sym].present?
 
       invalid_fields << {
         field: field.to_s,