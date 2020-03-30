alphagov/govuk-coronavirus-find-support

Change contact_details children keys to symbols in session

app/controllers/coronavirus_form/contact_details_controller.rb
@@ -7,13 +7,13 @@ class CoronavirusForm::ContactDetailsController < ApplicationController
 
   def submit
     contact_details = {
-      "phone_number_calls" => sanitize(params[:phone_number_calls]&.strip).presence,
-      "phone_number_texts" => sanitize(params[:phone_number_texts]&.strip).presence,
-      "email" => sanitize(params[:email]&.strip).presence,
+      phone_number_calls: sanitize(params[:phone_number_calls]&.strip).presence,
+      phone_number_texts: sanitize(params[:phone_number_texts]&.strip).presence,
+      email: sanitize(params[:email]&.strip).presence,
     }
     session[:contact_details] = contact_details
 
-    invalid_fields = contact_details["email"] ? validate_email_address("email", contact_details["email"]) : []
+    invalid_fields = contact_details[:email] ? validate_email_address("email", contact_details[:email]) : []
 
     if invalid_fields.any?
       flash.now[:validation] = invalid_fields

alphagov/govuk-coronavirus-vulnerable-people-form

app/controllers/coronavirus_form/contact_details_controller.rb
@@ -3,13 +3,13 @@
 class CoronavirusForm::ContactDetailsController < ApplicationController
   def submit
     contact_details = {
-      "phone_number_calls" => strip_tags(params[:phone_number_calls]&.strip).presence,
-      "phone_number_texts" => strip_tags(params[:phone_number_texts]&.strip).presence,
-      "email" => strip_tags(params[:email]&.strip).presence,
+      phone_number_calls: strip_tags(params[:phone_number_calls]&.strip).presence,
+      phone_number_texts: strip_tags(params[:phone_number_texts]&.strip).presence,
+      email: strip_tags(params[:email]&.strip).presence,
     }
     session[:contact_details] = contact_details
 
-    invalid_fields = contact_details["email"] ? validate_email_address("email", contact_details["email"]) : []
+    invalid_fields = contact_details[:email] ? validate_email_address("email", contact_details[:email]) : []
 
     if invalid_fields.any?
       flash.now[:validation] = invalid_fields