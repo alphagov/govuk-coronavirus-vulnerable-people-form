alphagov/govuk-coronavirus-find-support

Change name children keys to symbols in session

app/controllers/coronavirus_form/name_controller.rb
@@ -8,9 +8,9 @@ class CoronavirusForm::NameController < ApplicationController
 
   def submit
     session[:name] ||= {}
-    session[:name]["first_name"] = sanitize(params[:first_name]&.strip).presence
-    session[:name]["middle_name"] = sanitize(params[:middle_name]&.strip).presence
-    session[:name]["last_name"] = sanitize(params[:last_name]&.strip).presence
+    session[:name][:first_name] = sanitize(params[:first_name]&.strip).presence
+    session[:name][:middle_name] = sanitize(params[:middle_name]&.strip).presence
+    session[:name][:last_name] = sanitize(params[:last_name]&.strip).presence
 
     invalid_fields = validate_text_fields(%w[first_name last_name], "name")
 
@@ -32,7 +32,7 @@ private
 
   def validate_text_fields(mandatory_fields, page)
     mandatory_fields.each_with_object([]) do |field, invalid_fields|
-      next if session[:name][field].present?
+      next if session[:name][field.to_sym].present?
 
       invalid_fields << { field: field.to_s,
                           text: t("coronavirus_form.questions.#{page}.#{field}.custom_error",

alphagov/govuk-coronavirus-vulnerable-people-form

app/controllers/coronavirus_form/name_controller.rb
@@ -8,9 +8,9 @@ class CoronavirusForm::NameController < ApplicationController
 
   def submit
     session[:name] ||= {}
-    session[:name]["first_name"] = strip_tags(params[:first_name]&.strip).presence
-    session[:name]["middle_name"] = strip_tags(params[:middle_name]&.strip).presence
-    session[:name]["last_name"] = strip_tags(params[:last_name]&.strip).presence
+    session[:name][:first_name] = strip_tags(params[:first_name]&.strip).presence
+    session[:name][:middle_name] = strip_tags(params[:middle_name]&.strip).presence
+    session[:name][:last_name] = strip_tags(params[:last_name]&.strip).presence
 
     invalid_fields = validate_text_fields(%w[first_name last_name], controller_name)
 
@@ -32,7 +32,7 @@ private
 
   def validate_text_fields(mandatory_fields, page)
     mandatory_fields.each_with_object([]) do |field, invalid_fields|
-      next if session[:name][field].present?
+      next if session[:name][field.to_sym].present?
 
       invalid_fields << { field: field.to_s,
                           text: t("coronavirus_form.questions.#{page}.#{field}.custom_error",