7733e0afec79701bbe308d81539953f518be70fc
Graham Lewis <graham.lewis@digital.cabinet-office.gov.uk>
2020-04-28 15:52:23 +0100

    Backend integration with postcode API
    GIVEN I have an address
    THEN we will need to associate it with a UPRN
    
    Local authorities will need to match these against their own databases and
    they need UPRNs for this - we can't infer these from postcodes alone as
    UPRNs are residence specific.
    
    We need a way for a specific UPRN to be selected from the list of available
    addresses that are associated with a postcode.
    
    The change adds a backend API call to the Ordnance Survey Places API
    which returns a list of UPRNs and specific address for a given postcode.

---
+++ app/helpers/address_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module AddressHelper
+  API_URL = "https://api.ordnancesurvey.co.uk/places/v1/addresses/postcode?dataset=LPI"
+
+  def postcode_lookup(postcode)
+    postcode_url = "#{API_URL}&postcode=#{postcode}&key=#{ENV['ORDNANCE_SURVEY_PLACES_API_KEY']}"
+
+    response = Faraday.get(postcode_url)
+    details = JSON.parse(response.body)
+
+    addresses = details["results"]
+    addresses.map do |address|
+      { address.dig("LPI", "UPRN") => address.dig("LPI", "ADDRESS") }
+    end
+  end
+end
