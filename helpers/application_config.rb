5af793b9857f5fb2365b841456c39c0b68e23676
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-05-01 13:26:12 +0100

    [WIP] Remove basic auth from /metrics

--- helpers/application_config.rb
+++ helpers/application_config.rb
@@ -7,9 +7,9 @@ class ApplicationConfig
       use Prometheus::Middleware::Collector
 
       map "/metrics" do
-        use Rack::Auth::Basic, "Prometheus Metrics" do |username, password|
-          Rack::Utils.secure_compare(Rails.application.config.metrics_username, username) && Rack::Utils.secure_compare(Rails.application.config.metrics_password, password)
-        end
+        # use Rack::Auth::Basic, "Prometheus Metrics" do |username, password|
+        #   Rack::Utils.secure_compare(Rails.application.config.metrics_username, username) && Rack::Utils.secure_compare(Rails.application.config.metrics_password, password)
+        # end
         use Rack::Deflater
         use Prometheus::Middleware::Exporter, path: ""
         run ->(_) { [500, { "Content-Type" => "text/html" }, ["Metrics endpoint is unreachable!"]] }
