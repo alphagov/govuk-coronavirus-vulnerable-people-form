3ad9267d885b2a235219250c386e0e0639f17c41
Leena Gupte <leena.gupte@digital.cabinet-office.gov.uk>
2020-06-03 12:55:46 +0100

    Preserve query params when redirecting from /start
    Trello: https://trello.com/c/4LpwmmCy
    
    What's changed?
    ---------------
    
    Pass any and all query string parameters from `/start` to the first
    question.
    
    For example: `/start?foo=123` now redirects to
    `/live-in-england?foo=123` rather than `/live-in-england`
    
    If no query params are passed in, `/start` redirects to
    `/live-in-england` as before.
    
    Why?
    ----
    
    We need to preserve the `_ga` parameter from GOV.UK to the service for
    cross domain tracking to work.
    The original 301 redirect broke this. I think it's because
    analytics.js.erb isn't being called until a page in the form is loaded,
    by which point we'd already lost the query params with the `_ga`
    parameter.
    
    How to test locally
    -------------------
    
    Run the app:
    
    `foreman start`
    
    Go to `localhost:5000/start?foo=123`
    
    You should be redirected to `localhost:5000/live-in-england?foo=123`

--- spec/requests/start_spec.rb
+++ spec/requests/start_spec.rb
@@ -4,5 +4,11 @@ RSpec.describe "start" do
       get start_path
       expect(response).to redirect_to live_in_england_path
     end
+
+    it "preserves query parameters" do
+      get start_path, params: { foo: "123" }
+
+      expect(response).to redirect_to live_in_england_path(foo: "123")
+    end
   end
 end
