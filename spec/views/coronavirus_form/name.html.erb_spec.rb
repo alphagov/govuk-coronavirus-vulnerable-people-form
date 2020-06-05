cd82073e8016720ae83082e5abb3e8c218be4039
Bill Franklin <william.franklin@digital.cabinet-office.gov.uk>
2020-06-05 10:49:15 +0100

    Preserve user-provided name fields in name view
    This fixes a bug, where a user wouldn't be shown the names they
    had entered thanks to us using Strings instead of Symbols.
    
    https://trello.com/c/AQs4RW9M/547-if-you-error-on-name-page-it-loses-any-data-you-entered-on-that-page

---
+++ spec/views/coronavirus_form/name.html.erb_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe "coronavirus_form/name" do
+  before do
+    allow(view).to receive(:previous_path).and_return("/")
+  end
+
+  context "when form fields have already been provided" do
+    it "shows the filled in fields" do
+      name = {
+        first_name: "Harry",
+        middle_name: "",
+        last_name: "Potter",
+      }
+      assign(:form_responses, name: name)
+
+      render
+      name.values.each do |txt|
+        expect(rendered).to have_selector("input[value='#{txt}']")
+      end
+    end
+  end
+end
